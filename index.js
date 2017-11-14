0a0f4d359f248f741f742cd9791de8177a492aed
Marcos Mellado <marcos@mellados.com>
2017-11-14 12:05:57 +0100

    Update rules to include order

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-"use strict"
+'use strict';
 
-module.exports = require("./stylelint.config.json")
+var style = require('./rules/style.js');
+var order = require('./rules/order.js');
+var rules = Object.assign(style, order);
+
+module.exports = {
+  plugins: ['stylelint-scss', 'stylelint-order'],
+  rules: rules,
+};
--- test/stylelint.spec.js
+++ test/stylelint.spec.js
@@ -1,7 +1,8 @@
 import { lint } from 'stylelint';
+const stylelintConfig = require('../index.js');
 
 describe('stylelint', () => {
-  it('should validate the .stylelint rules', done => {
+  it('should use the stylelint config', done => {
     const code = `
 body {
   background: #000000;
@@ -10,15 +11,150 @@ body {
 
     lint({
       code,
-      configFile: './stylelint.config.json',
-    })
-      .then(res => {
-        expect(res.errored).toBe(false);
-        done();
-      })
-      .catch(err => {
-        console.log(err.stack);
-        done();
-      });
+      config: stylelintConfig,
+    }).then(res => {
+      expect(res.errored).toBe(false);
+      done();
+    });
+  });
+
+  it('should use the right priority for rule groups', done => {
+    const codeWrong = `
+body {
+  box-sizing: border-box;
+
+  list-style: none;
+
+  content: 'foo';
+
+  opacity: 0;
+
+  font-size: 10px;
+
+  display: none;
+}
+    `;
+
+    const codeCorrect = `
+body {
+  display: none;
+
+  box-sizing: border-box;
+
+  list-style: none;
+
+  content: 'foo';
+
+  opacity: 0;
+
+  font-size: 10px;
+}
+    `;
+
+    lint({
+      code: codeWrong,
+      config: stylelintConfig,
+    }).then(res => {
+      expect(res.errored).toBe(true);
+      done();
+    });
+
+    lint({
+      code: codeCorrect,
+      config: stylelintConfig,
+    }).then(res => {
+      expect(res.errored).toBe(false);
+      done();
+    });
+  });
+
+  it('should use the right order per group', done => {
+    const codeRight = `
+body {
+  display: none;
+  visibility: hidden;
+  float: left;
+}
+    `;
+
+    const codeWrong = `
+ body {
+  display: none;
+  float: left;
+  visibility: hidden;
+ }
+    `;
+
+    lint({
+      code: codeRight,
+      config: stylelintConfig,
+    }).then(res => {
+      expect(res.errored).toBe(false);
+      done();
+    });
+
+    lint({
+      code: codeWrong,
+      config: stylelintConfig,
+    }).then(res => {
+      expect(res.errored).toBe(true);
+      done();
+    });
+  });
+
+  it('should respect order for variables, includes and media queries', done => {
+    const codeRight = `
+body {
+  $foo: var;
+
+  @include foo();
+  @include bar();
+
+  display: none;
+
+  @include respond-to('foo') {
+    display: block;
+  }
+
+  @media screen {
+    opacity: .5;
+  }
+}
+    `;
+
+    const codeWrong = `
+ body {
+  $foo: var;
+
+  @include respond-to('foo') {
+    display: block;
+  }
+
+  @media screen {
+    opacity: .5;
+  }
+
+  display: none;
+
+  @include foo();
+  @include bar();
+ }
+    `;
+
+    lint({
+      code: codeRight,
+      config: stylelintConfig,
+    }).then(res => {
+      expect(res.errored).toBe(false);
+      done();
+    });
+
+    lint({
+      code: codeWrong,
+      config: stylelintConfig,
+    }).then(res => {
+      expect(res.errored).toBe(true);
+      done();
+    });
   });
 });
